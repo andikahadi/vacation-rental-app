85a89fcaedbbe5d13dfa453b77a69c3a038b9b9c
andikahadisaputra <andikahadisaputra@gmail.com>
2023-07-22 03:24:42 +0800

    create navbar template and reusable container for padding x responsive

---
+++ vacationrental/app/components/Container.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return (
+    <div
+      className="
+        max-w-[1920px]
+        mx-auto
+        px-4
+        md:px-8
+      "
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
--- vacationrental/app/components/navbar/Navbar.tsx
+++ vacationrental/app/components/navbar/Navbar.tsx
@@ -1,7 +1,14 @@
+import Container from "../Container";
+
 const Navbar = () => {
   return (
-    <div className="py-5 bg-teal-500">
-      <div> </div>
+    <div className="bg-teal-500 h-40 fixed z-10 w-full">
+      <div className="py-5 border-b-2">
+        <Container>
+          <div>hello</div>
+        </Container>
+      </div>
+      <div className="py-5 border-b-2"> I am navbar bottom</div>
     </div>
   );
 };
